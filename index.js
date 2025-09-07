206bd38afde2d9efae20a6a8e995d01227f4a96e
Aryan Chauhan <174147041+arychauhann@users.noreply.github.com>
2025-09-07 10:14:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function checkMaintenanceMode(req, res, next) {
   next();
 }
 
-mongoose.connect('mongodb+srv://motame7485_db_user:1Pvz4F8QcKkKJc8p@goatmart.gn83d9x.mongodb.net/?retryWrites=true&w=majority&appName=GoatMart')
+mongoose.connect('mongodb+srv://lajegix672:KaXY8RqzIcJ31Nyo@goatmart.9p854kx.mongodb.net/?retryWrites=true&w=majority&appName=GoatMart')
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
